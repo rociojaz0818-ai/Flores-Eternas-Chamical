d97ed9f35778c62b22203dcb5918e35ef276cb94
rociojaz0818-ai <rociojaz.0818@gmail.com>
2026-09-07 00:14:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
-/* =====================================
-   🌸 LLUVIA DE FLORES
-===================================== */
+const flower = document.createElement("div");
 
-function createFlower() {
+flower.innerHTML = "🌸";
 
-    const flower = document.createElement("div");
+flower.style.position = "fixed";
+flower.style.top = "50px";
+flower.style.left = "50%";
+flower.style.fontSize = "50px";
+flower.style.zIndex = "99999";
 
-    flower.classList.add("flower-rain");
-
-    const flowers = ["✿", "✿", "✿", "✿", "✿"];
-
-    flower.innerHTML = flowers[
-        Math.floor(Math.random() * flowers.length)
-    ];
-
-    flower.style.left = Math.random() * 100 + "vw";
-
-    flower.style.fontSize =
-        (14 + Math.random() * 12) + "px";
-
-    flower.style.animationDuration =
-        (6 + Math.random() * 6) + "s";
-
-    flower.style.transform =
-        `rotate(${Math.random() * 360}deg)`;
-
-    document.body.appendChild(flower);
-
-    setTimeout(() => {
-        flower.remove();
-    }, 13000);
-}
-
-setInterval(createFlower, 700);
+document.body.appendChild(flower);
