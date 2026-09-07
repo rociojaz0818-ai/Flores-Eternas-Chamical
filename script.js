afea23c5c83a90aaee2572bfbd23ab3b33798b2e
rociojaz0818-ai <rociojaz.0818@gmail.com>
2026-09-07 00:15:52 -0300

    Remove flower creation and add alert

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const flower = document.createElement("div");
-
-flower.innerHTML = "🌸";
-
-flower.style.position = "fixed";
-flower.style.top = "50px";
-flower.style.left = "50%";
-flower.style.fontSize = "50px";
-flower.style.zIndex = "99999";
-
-document.body.appendChild(flower);
+alert("EL JAVASCRIPT FUNCIONA");
